e52028845aee164a8f74a7663e14b91f40f2b4e3
Jericho Huelar <huelarjl18@gmail.com>
2026-08-31 22:36:21 -0600

    TA-09-a/Footer: Add Powered by Nullus

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,6 +20,23 @@ export function Footer() {
           className="object-contain"
         />
       </div>
+      <div className="absolute right-1/20 top-1/5 bg-primary w-[28%] h-[35%] flex flex-col items-center justify-center text-white">
+        <p className="mt-8 mb-1 font-light">Powered by</p>
+        <a
+          href="https://www.nullus.ca/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/images/branding/NULLUS_FULL_LOGO.svg"
+            alt="Nullus Inc. Logo"
+            width={800}
+            height={800}
+            priority
+            className="h-12 w-auto mx-2 brightness-0 invert"
+          />
+        </a>
+      </div>
     </footer>
   );
 }
