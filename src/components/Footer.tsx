c2ca7cd3be5edb06d1dd67e8a3b13c164cee574d
Jericho Huelar <huelarjl18@gmail.com>
2026-08-31 22:09:09 -0600

    TA-09-a/Footer: Add CISA info box

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,14 @@
 
 export function Footer() {
   return (
-    <footer className="footer-grid">
+    <footer className="footer-grid relative">
+      <div className="absolute left-1/20 top-1/5 bg-primary w-[28%] h-[35%] flex items-center justify-center text-white">
+        <div className="text-left font-bold *:text-lg *:sm:text-xl *:md:text-2xl *:lg:text-3xl">
+          <div>@saplcisa</div>
+          <div>hello@saplcisa.com</div>
+          <div>801 7 Ave SW, room 713</div>
+        </div>
+      </div>
     </footer>
   );
 }
