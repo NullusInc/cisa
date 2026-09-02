4e8b2e69996dad19fd7f8d7b42991d6f6202293b
Jericho Huelar <huelarjl18@gmail.com>
2026-09-01 22:42:21 -0600

    TA-09-a/Footer: Move Powered by Nullus to info box

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,11 +5,22 @@ import Image from 'next/image';
 export function Footer() {
   return (
     <footer className="footer-grid relative">
-      <div className="absolute left-1/20 top-1/5 bg-primary w-[28%] h-[35%] flex items-center justify-center text-white">
-        <div className="text-left font-bold *:text-lg *:sm:text-xl *:md:text-2xl *:lg:text-3xl">
+      <div className="absolute left-1/20 top-1/5 bg-primary w-[28%] h-[70%] flex flex-col items-center justify-center text-white">
+        <div className="flex flex-col gap-1 text-left *:text-[clamp(1.125rem,2.2vw,1.875rem)]">
           <div>@saplcisa</div>
           <div>hello@saplcisa.com</div>
-          <div>801 7 Ave SW, room 713</div>
+          <div>801 7 Ave AW, room 713</div>
+          <p className="mt-8 mb-1 font-light">Powered by</p>
+          <a href="https://www.nullus.ca/" target="_blank" rel="noopener noreferrer">
+            <Image
+              src="/images/branding/NULLUS_FULL_LOGO.svg"
+              alt="Nullus Inc. Logo"
+              width={800}
+              height={800}
+              priority
+              className="h-[clamp(2rem,4.5vw,3rem)] w-auto brightness-0 invert"
+            />
+          </a>
         </div>
       </div>
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-[90%] aspect-square">
@@ -19,23 +30,6 @@ export function Footer() {
           fill
           className="object-contain"
         />
-      </div>
-      <div className="absolute right-1/20 top-1/5 bg-primary w-[28%] h-[35%] flex flex-col items-center justify-center text-white">
-        <p className="mt-8 mb-1 font-light">Powered by</p>
-        <a
-          href="https://www.nullus.ca/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/images/branding/NULLUS_FULL_LOGO.svg"
-            alt="Nullus Inc. Logo"
-            width={800}
-            height={800}
-            priority
-            className="h-12 w-auto mx-2 brightness-0 invert"
-          />
-        </a>
       </div>
     </footer>
   );
