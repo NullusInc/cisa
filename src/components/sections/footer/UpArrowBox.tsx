dc8a608392e3c04d66e9a4de6fe70aabff87e509
Jericho Huelar <huelarjl18@gmail.com>
2026-09-02 20:56:22 -0600

    TA-09-a/Footer:  Add reviewed changes
    *Added icons
    *Layout changed so footer applies to every page
    *Made UpArrow SVG individual component
    *Powered by smaller to emphasize Nullus
     *Add text-background instead of text-white

---
+++ src/components/sections/footer/UpArrowBox.tsx
@@ -0,0 +1,18 @@
+export function UpArrowBox() {
+  return (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-4/5 w-4/5"
+        >
+          <path d="M12 19V5" />
+          <path d="M5 12l7-7 7 7" />
+    </svg>
+
+  );
+}
