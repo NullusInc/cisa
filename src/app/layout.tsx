18d78d641977985916bfdbb8cccbfc72ab3590ff
Alexandre Duteau <alex@midaslabs.co>
2026-09-05 17:27:23 -0600

    Fixed text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export const metadata: Metadata = {
     title: "City Innovation Students' Association",
     description:
       "The City Innovation Students' Association (CISA) is the undergraduate government body with the mission to represent academic student interests and foster an inclusive community within the School of Architecture, Planning and Landscape.",
-    creator: '@cisaryerson',
+    creator: '@saplcisa',
   },
   robots: 'index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1',
   alternates: {
