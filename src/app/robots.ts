92d193d166a99199a9426e3aa3ec45539e75d1f0
Alexandre Duteau <alex@midaslabs.co>
2026-09-05 17:28:50 -0600

    Encahgned the robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,9 +4,30 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: [
       {
-        userAgent: '*',
+        userAgent: 'Googlebot',
+        allow: '/',
+        crawlDelay: 0.5,
+      },
+      {
+        userAgent: 'Bingbot',
         allow: '/',
         crawlDelay: 1,
+      },
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: [
+          '/*.pdf',
+          '/*.zip',
+          '/*.exe',
+          '/*.tar',
+          '/*.gz',
+          '/admin',
+          '/private',
+          '/.env',
+          '/.git',
+        ],
+        crawlDelay: 2,
       },
     ],
     sitemap: 'https://saplcisa.ca/sitemap.xml',
